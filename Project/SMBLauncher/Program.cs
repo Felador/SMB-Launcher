fb5553f4b90e5762745ccd746fe4ad7f3fdc985f
Felador <51974037+Felador@users.noreply.github.com>
2019-06-18 23:46:07 +0100

    Correctly added project files

---
+++ Project/SMBLauncher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMBLauncher
+{
+    class Program
+    {
+
+        public static int _ID = 3;
+
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new MainForm());
+        }
+    }
+}
